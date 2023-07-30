8dc0b699117cae1cb01abed7f6ba3cbca82fdb1a
valere chihisa <vchihisa@gmail.com>
2023-07-30 11:45:20 +0200

    minor update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://www.atelier.mdundiko.xyz"],
+    origin: ["https://uzisha-shop.vercel.app"],
     credentials: true,
   })
 );
